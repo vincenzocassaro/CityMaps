70da86245fe33efb7d699f438bbef93f45eb7ed5
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2022-08-17 17:10:38 +0200

    readme, template html

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # CityMaps
+
+## How To
+1. Andare su prettymaps nel file main mattere il nome della citta desiderata a riga 18 (e a 90 e 91 il nome con cui si vogliono salvare i file)
+
+2. andare nel progetto citymaps (questo) e fare una copia del file template.html e rinominarlo ad esempio col nome della citta che si sta facendo
+
+3. aprire il file svg e copiare dal tag svg in poi
+
+4. incollare nel file nome_citta.html nel punto indicato il codice svg
+
+5. aggiungere il css con l'animazione
+
+6. aprire nel browser il file nome_citta.html
+
+7. alla fine dell'animazione verra chiesto dove si vuole salvare il video
+
+8. run command ```python generate.py ``` per generare un pezzo di video con l'immagine completa e colorata
+
+9. run command ```python mergevideos.py ``` per mergiare i due video insieme
+
+10. vai su tiktok, pubblica, make profit
--- src/addattribute.js
+++ src/addattribute.js
@@ -6,7 +6,7 @@ function addAttribbute(path) {
     path.setAttribute("stroke-dashoffset", length);
     path.setAttribute("fill-desired", path.style.fill)
     path.style.fill = "#00000000"
-    path.style.animation = "strokeoffset 7000ms ease-in-out 1 1000ms normal forwards running"
+    path.style.animation = "strokeoffset 15000ms ease-in-out 1 1000ms normal forwards running"
 }
 
 window.addEventListener('load', (event) => {
@@ -20,4 +20,165 @@ window.addEventListener('load', (event) => {
     }
 
     console.log("finivu");
+
+
+
+    /* Utils */
+
+    function svg2canvas(svg, ctx, callback) {
+        const serialized = new XMLSerializer().serializeToString(svg),
+            url = URL.createObjectURL(new Blob([serialized], { type: "image/svg+xml" }));
+
+        const img = new Image();
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0);
+            callback();
+        };
+        img.src = url;
+    }
+
+
+    class CanvasRecorder {
+        constructor(options) {
+            const canvas = options.canvas,
+                callback = options.callback;
+
+            let framerate = options.framerate || 0;
+            //Setting the framerate to 0 lets us render each frame manually through requestFrame(),
+            //but only if the browser supports it (2018: Only Chrome, Firefox behind a flag):
+            //https://developer.mozilla.org/en-US/docs/Web/API/CanvasCaptureMediaStreamTrack#Methods
+            this.hasRequestFrame = (typeof CanvasCaptureMediaStreamTrack !== 'undefined') &&
+                ('requestFrame' in CanvasCaptureMediaStreamTrack.prototype);
+            console.log('requestFrame() support', this.hasRequestFrame);
+            if ((framerate === 0) && !this.hasRequestFrame) {
+                //Without the manual control over framerate, just use a suitable default:
+                framerate = 24;
+            }
+
+            //https://developer.mozilla.org/en-US/docs/Web/API/CanvasCaptureMediaStreamTrack/requestFrame#Example
+            const stream = canvas.captureStream(framerate),
+                track = stream.getVideoTracks && stream.getVideoTracks()[0];
+
+            const chunks = [],
+                recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+
+            recorder.ondataavailable = function (e) {
+                const blob = e.data;
+                if (blob && blob.size) { chunks.push(blob); }
+            };
+
+            recorder.onstop = (e) => {
+                //console.log('stop', e);
+                const url = URL.createObjectURL(new Blob(chunks, { type: "video/webm" }));
+                callback(url);
+            };
+
+            this.recorder = recorder;
+            this.track = track;
+        }
+
+        start() {
+            this.recorder.start();
+        }
+
+        stop() {
+            this.recorder.stop();
+        }
+
+        requestFrame() {
+            if (!this.hasRequestFrame) { return; }
+            this.track.requestFrame();
+        }
+    }
+
+    function downloadURI(uri, name) {
+        var link = document.createElement("a");
+        link.download = name;
+        link.href = uri;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        delete link;
+    }
+
+    const svg = document.querySelector('svg'),
+        canv = document.querySelector('#rendered'),
+        ctx = canv.getContext('2d');
+
+
+    //Prepare a canvas on which to render the frames for our video
+
+    //Firefox requires fixed width/height on the SVG to render it on a canvas
+    //..and also doesn't support .clientWidth/Height on SVG(!)
+    //https://bugzilla.mozilla.org/show_bug.cgi?id=874811
+    canv.width = svg.clientWidth;
+    canv.height = svg.clientHeight;
+    const recorder = new CanvasRecorder({
+        canvas: canv,
+        callback: url => {
+            const video = document.body.appendChild(document.createElement('video'));
+            video.src = url;
+            video.controls = true;
+            video.autoplay = true;
+
+            downloadURI(url, "helloWorld.mp4");
+        },
+    });
+
+
+    //Find all animated elements, and save their original animation-delay:
+    var animed = Array.from(document.querySelectorAll('*')).filter(x => x.style.animationName);
+    animed.forEach(x => {
+        var css = x.style,
+            anim = css.animationName,
+            delay = css.animationDelay;
+
+        //console.log(x.id, anim, delay);
+        x.__originalDelay = delay.match(/\d/) ? delay : '0s';
+    });
+
+
+    //Loop through all animated elements, and update their animation-delay.
+    //Together with "animation-play-state: paused", this freezes the animation at the specified time.
+    function freeze(time) {
+        animed.forEach(x => {
+            x.style.animationPlayState = 'paused';
+            x.style.animationDelay = `calc(${x.__originalDelay} - ${time}ms)`;
+        });
+    }
+
+
+    function render(time, frame, callback) {
+        //20fps is more than enough..
+        // if ((frame % 1) !== 1) { callback(); return; }
+
+        //console.log('rendering', frameNum, time);
+        freeze(time);
+        svg2canvas(svg, ctx, () => {
+            //console.log('  ..requesting');
+            recorder.requestFrame();
+            callback();
+        });
+    }
+
+    let startT, frameNum = 0;
+    (function renderLoop(t) {
+        if (!t) { requestAnimationFrame(renderLoop); return; }
+
+        if (!startT) {
+            startT = t;
+            recorder.start();
+        }
+        let animTime = (t - startT);
+
+        //The complete animation lasts around 10 seconds:
+        if (animTime > 15000) {
+            recorder.stop();
+            return;
+        }
+
+        frameNum++;
+        render(animTime, frameNum, () => requestAnimationFrame(renderLoop));
+    })();
+
 })
